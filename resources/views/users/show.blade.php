e7fe926b5703d3da0a3776cefd3694bd6804d26d
kevinjimmy-dev <kevinjimmy.profissional@hotmail.com>
2022-04-14 15:44:16 -0300

    Views atualizadas com o framework TailWhind

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,11 +3,13 @@
 @section('title', 'Listagem do Usuário')
 
 @section('content')
-    <h1>Listam do Usuário {{ $user->name }}</h1>
+
+    <h1 class="text-2xl font-semibold leading-tigh py-2">Listagem do usuário {{ $user->name }}</h1>
 
     <ul>
         <li>{{ $user->name }}</li>
         <li>{{ $user->email }}</li>
     </ul>
+
 @endsection
 
